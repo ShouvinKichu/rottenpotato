00bd3cd9a527c90a00fb1986a42f200363ec8398
Shouvin.A <72617116+ShouvinKichu@users.noreply.github.com>
2024-04-04 22:11:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=ea1f79f9'
+const API_URL = 'https://www.omdbapi.com?apikey=ea1f79f9'
 
 const App = () => {
 
